b159e86895928c5970e20c1de8bc314280695ad0
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-02-12 16:37:45 -0800

    created folders for portfolio

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <>
-     <header>
-     Components 
-     </header> 
-     <main>
-       main
-     </main>
-
-     <footer>
-       footer
-     </footer>
+      <NavBar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    
     </>
   );
 }
