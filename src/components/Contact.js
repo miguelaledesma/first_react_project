f56c443d115a925509d70950a12ea9bb9d8f3963
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-17 21:50:54 -0700

    wired contact me form, i can now recieve inquiries to my email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 
-// const FORM_ENDPOINT = 
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/49f84c80-a670-11ec-975b-f9cc2b0e6900"
 
 const Contact = () => {
     const [submitted, setSubmitted] = useState(false)
 
     const onSubmit = (evt) => {
         evt.preventDefault()
+        setTimeout(() => {
+            setSubmitted(true); 
+        }, 100); 
 
+    }; 
+
+    if(submitted){
+        return (
+            <>
+            <h4>Thank you!</h4>
+            <div>I will contact you soon.</div>
+            </>
+        )
+    } else if (submitted){
+        return setSubmitted(true)
     }
 
     return(
         <section id= 'contact'>
             
             <h2>contact me</h2>
-           <form onSubmit = {onSubmit} method = "POST" target = "_blank">
+           <form onSubmit = {onSubmit} method = "POST" target = "_blank" action = {FORM_ENDPOINT}>
                <div className = 'name'>
                <input 
                type = "text" 
@@ -32,7 +46,7 @@ const Contact = () => {
                </div>
 
                <div >
-               <textarea
+               <input
                className = 'message'
                type = "text"
                placeholder = "Message me!"
